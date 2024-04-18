7efa7cf24312c7ada9fa00545f99d07546f30f6e
Luís Arruda <up202206970@up.pt>
2024-04-18 13:20:50 +0100

    detect OS dark mode

--- pages/script.js
+++ pages/script.js
@@ -1,3 +1,7 @@
+localStorage.setItem('darkmode', window.matchMedia("(prefers-color-scheme: dark)").matches);
+const element = document.body;
+element.classList.toggle('dark-mode', window.matchMedia("(prefers-color-scheme: dark)").matches);
+
 function darkmode() {
     const wasDarkmode = localStorage.getItem('darkmode') === 'true';
     localStorage.setItem('darkmode', !wasDarkmode);
