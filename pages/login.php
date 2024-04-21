7e58659ee949308af88383d5a581bcd0517527f3
Afonso-Machado <up202207611@up.pt>
2024-04-21 21:50:47 +0100

    Finished Login HTML + CSS

---
+++ pages/login.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Retro Club</title>
+        <link rel="stylesheet" href="../css/style.css">
+        <script src="script.js" defer></script>
+    </head>
+    <body>
+        <header>
+            <a href="homepage.php"> 
+                <img class = "logo" src="../assets/Retro Club Logotipo.png" alt="logo"/>           
+            </a>
+
+            <section class="search-bar">
+                <button id="filter-button"><img src="../assets/filter.png" alt="filter"></button>
+                <input type="text" placeholder="Search here..."/>
+                <button id="search-button"><img src="../assets/search.png" alt="search"></button>
+            </section> 
+
+            <nav class="buttons">
+                <a href="homepage.php">Home</a>
+                <a href="sell.php">Sell</a>
+                <a href="wishlist.php">
+                    <img src="../assets/wishlist.svg" alt="wishlist"/>
+                </a>
+                <a href="profile.php">
+                    <img src="../assets/profile.svg" alt="profile"/>
+                </a>
+                <a href="shopping_cart.php">
+                    <img src="../assets/cart.svg" alt="shopping cart"/>
+                </a>
+            </nav>
+            
+            <nav class="categories">
+                <ul>
+                    <li><a href="categories.php">All Categories</a></li>
+                    <li><a href="#">Women</a></li>
+                    <li><a href="#">Men</a></li>
+                    <li><a href="#">Kids</a></li>
+                    <li><a href="#">House</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <form class="login_inputs">  
+                <img src="../assets/sad_disco_ball.png" alt="disco ball"/>
+                <section class="input_fields">
+                    <input class="input_info" type="text" name="text" placeholder="username">
+                    <input class="input_info" type="password" name="password" placeholder="password">
+                </section>
+                <button formaction="login.php" formmethod="post" type="submit">
+                    LOG IN
+                </button>
+                <p>Don't have an account yet? <a href="register.php"><mark>Sign Up.</mark></a></p>
+            </form>    
+        </main>
+        <footer>
+            Copyright &copy; 2024 Retro Club. All rights reserved.
+        </footer>    
+    </body>
+</html>
