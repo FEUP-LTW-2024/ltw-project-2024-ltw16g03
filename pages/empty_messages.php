fb5a9aeff7cb90f54ead268f2ac51fb8fb1f3fc8
g3raldes <g3raldes@gmail.com>
2024-05-04 08:46:47 +0100

    added empty_messages.php

---
+++ pages/empty_messages.php
@@ -0,0 +1,12 @@
+<?php
+  declare(strict_types = 1);
+
+  require_once(__DIR__ . '/../templates/common.tpl.php');
+  require_once(__DIR__ . '/../utils/session.php');
+  $session = new Session();
+?>
+  <?=drawClassicHeader();?>
+    
+  <h1 class="sub_title">MESSAGES</h1>
+  <?=drawEmpty("YOU DON'T HAVE ANY MESSAGES YET", "", false, false)?>
+  <?=drawFooter();?>
