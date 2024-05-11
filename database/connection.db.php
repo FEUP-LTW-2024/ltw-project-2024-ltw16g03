4b79c436c14a655029f7c8ca6bc7b79e040c1e3b
Afonso-Machado <up202207611@up.pt>
2024-05-11 23:42:19 +0100

    Fixed bug where items where not being deleted along with the owner account

--- database/connection.db.php
+++ database/connection.db.php
@@ -6,6 +6,9 @@
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    // Enable foreign key constraints
+    $db->exec('PRAGMA foreign_keys = ON');
+
     return $db;
   }
 ?>
