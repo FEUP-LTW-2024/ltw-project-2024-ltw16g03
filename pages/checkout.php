623d2c155542d308f32a55edab963213d799fdeb
g3raldes <g3raldes@gmail.com>
2024-05-15 00:58:12 +0100

    Added some things to checkout.php and process_order.php

--- pages/checkout.php
+++ pages/checkout.php
@@ -1,83 +1,67 @@
-<!DOCTYPE html>
-<html lang="en-US">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Retro Club</title>
-        <link rel="stylesheet" href="../css/style.css">
-        <script src="script.js" defer></script>
-    </head>
-    <body>
-        <header class="classic">
-            <a href="homepage.php"> 
-                <img class = "logo" src="../assets/Retro Club Logotipo.png" alt="logo"/>           
-            </a>
-            <h1>THANK YOU!</h1>                       
-        </header>
+<?php
+  require_once(__DIR__ . '/../utils/session.php');
+  $session = new Session();
+
+  require_once(__DIR__ . '/../database/connection.db.php');
+  require_once(__DIR__ . '/../database/item.class.php'); 
+
+  require_once(__DIR__ . '/../templates/common.tpl.php');
+  $db = getDatabaseConnection();
+
+  $cartItems = $session->getItemsInCart();
+  $shippingInfo = isset($_SESSION['shipping']) ? $_SESSION['shipping'] : array();
+  unset($_SESSION['shipping']);
+?>
+
+<?=drawClassicHeader("THANK YOU");?>
         <main>
             <section class="process-order">
                 <article class="shipping-left2">
                     <h1 class="shipping-titles2">SHIPPING</h1>
                     <div class="shipping-container"> 
-                        First Name Last Name
-                        Address
-                        State, Postal Code
-                        City
+                    <?php foreach ($shippingInfo as $value) : ?>
+                        <p><?= $value ?></p>
+                    <?php endforeach; ?>
                     </div>
                 </article>
                 <article class="shipping-right2">
                     <h1 class="shipping-titles3">PAYMENT</h1>
                     <div class="payment-option">
-                        <label for="debit-card">Debit or Credit Card</label>
-                        <img src="../assets/mastercard.png" alt="Debit Card" height = "50" width = "80">
+                    
                     </div>
                 </article>
             </section>
-            <h1 class="shipping-titles">ORDER</h1>
             <section class="item-checkout">
-                <article class="item-background">
-                    <img class="big-image-item" src="https://picsum.photos/500" alt="template" height = "250" width = "250"/>
-                    <img class="add-to-wishlist" src="../assets/wishlist.svg" alt="wishlist" height = "35" width = "35"/>
-                    <img class="eliminate-item" src="../assets/cross.svg" alt="cross" height = "40" width = "40"/>
-                    <p class="price2">20.00 $</p>
-                    <div class="info2">
-                        <p class="name2">Gelado</p>
-                        <div class="details">
-                            <div class="color-square2" style="background-color: yellow;"></div>
-                            <div class="size-square2" style="background-color: grey;">XS</div>
-                        </div>
-                    </div>
-                </article>
-            </section>    
-            <section class="item-checkout">
-                <article class="item-background">
-                    <img class="big-image-item" src="https://picsum.photos/500" alt="template" height = "250" width = "250"/>
-                    <img class="add-to-wishlist" src="../assets/wishlist.svg" alt="wishlist" height = "35" width = "35"/>
-                    <img class="eliminate-item" src="../assets/cross.svg" alt="cross" height = "40" width = "40"/>
-                    <p class="price2">20.00 $</p>
-                    <div class="info2">
-                        <p class="name2">Gelado</p>
-                        <div class="details">
-                            <div class="color-square2" style="background-color: yellow;"></div>
-                            <div class="size-square2" style="background-color: grey;">XS</div>
-                        </div>
-                    </div>
-                </article>
-            </section>     
+                <h1 class="shipping-titles">ITEMS</h1>
+                <?php 
+                    $total = 0;
+                    foreach ($cartItems as $item) { 
+                        $total += $item->Price;?>
+                        <article class="item-background">
+                            <img class="big-image-item" src="<?=$item->ImageURL?>" alt="template"/>
+                            <p class="price2"><?=$item->Price?> €</p>
+                            <div class="info2">
+                                <p class="name2"><?=$item->Detail?></p>
+                                <section class="details">
+                                    <span class="color-square2 <?=$item->Color?>"></span>
+                                    <span class="size-square2" style="background-color: grey;"><?=$item->Dimension?></span>
+                                </section>
+                            </div>
+                        </article>  
+                <?php } ?>
+            </section>
             <section class="total">
                 <article>
                     <div class="items-total">
-                        <p>2 items</p>
-                        <p class="big-total"><span class="bold-text">TOTAL</span> 20.00 $</p>
+                        <p><?=count($cartItems)?> items</p>
+                        <p class="big-total"><span class="bold-text">TOTAL</span> <?=$total?> €</p>
                     </div>
                 </article>
+            </section>
             </section>
             <div class="final-button">
                 <button>SHOP AGAIN</button>
             </div>
         </main>
-        <footer>
-            Copyright &copy; 2024 Retro Club. All rights reserved.
-        </footer>    
-    </body>
-</html>
+
+<?=drawFooter()?>
