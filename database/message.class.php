9a568c11fdc8d128ca0424c767ce41acdd2adab1
Afonso-Machado <up202207611@up.pt>
2024-05-15 23:03:06 +0100

    Changed order of chat appearence on chats pages (most recent at the top)

--- database/message.class.php
+++ database/message.class.php
@@ -31,7 +31,8 @@
         WHERE SenderID = :user_id OR ReceiverID = :user_id
         GROUP BY CorrespondentID
       ) AS latest_msg
-      ON (m.MessageID = latest_msg.MessageID)');
+      ON (m.MessageID = latest_msg.MessageID)
+      ORDER BY m.TimeStamp DESC;');
         
       $stmt->bindParam(':user_id', $id);
 
--- javascript/messages.js
+++ javascript/messages.js
@@ -2,32 +2,34 @@
 var form = document.getElementById('message_input');
 
 // Add event listener to the form's submit event
-form.addEventListener('submit', function(event) {
-    // Prevent the default form submission behavior
-    event.preventDefault();
-
-    // Get reference to input field
-    const inputField = this.querySelector(".input_info");
-    if (inputField.value.trim() == "") return;
-
-    // Build Message element
-    const messageElement = buildMessage.call(inputField);
+if (form) {
+    form.addEventListener('submit', function(event) {
+        // Prevent the default form submission behavior
+        event.preventDefault();
     
-    // Create a data object with the values to be sent to the PHP file
-    var data = {
-        receiverID: inputField.getAttribute('data-receiver'),
-        content: inputField.value
-    };
-
-    // Send an AJAX request to the PHP file
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '../api/send_message.php', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(data));
-
-    // Clear the input field if needed
-    inputField.value = '';
-});
+        // Get reference to input field
+        const inputField = this.querySelector(".input_info");
+        if (inputField.value.trim() == "") return;
+    
+        // Build Message element
+        const messageElement = buildMessage.call(inputField);
+        
+        // Create a data object with the values to be sent to the PHP file
+        var data = {
+            receiverID: inputField.getAttribute('data-receiver'),
+            content: inputField.value
+        };
+    
+        // Send an AJAX request to the PHP file
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '../api/send_message.php', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(data));
+    
+        // Clear the input field if needed
+        inputField.value = '';
+    });
+}
 
 function buildMessage() {
     const text = this.value;
